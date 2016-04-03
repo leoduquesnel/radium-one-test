61e07de301c0a2bfd064b6f8c6da0f347af5d19c
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 20:00:41 -0700

    retry cache in controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @stock_tickers = StockTicker.select(:id, :name, :updated_at).order('name asc').page params[:page]
+    cache @stock_tickers, expires_in: 5.minutes do
+      @stock_tickers = StockTicker.select(:id, :name, :updated_at).order('name asc').page params[:page]
+    end
   end
 end
