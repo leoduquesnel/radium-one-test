ae3f319f0b0b7a112954fa574ceaf3c84e1196ce
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 20:08:37 -0700

    remove cache on active record

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    cache @stock_tickers, expires_in: 5.minutes do
-      @stock_tickers = StockTicker.select(:id, :name, :updated_at).order('name asc').page params[:page]
-    end
+    @stock_tickers = StockTicker.select(:id, :name, :updated_at).order('name asc').page params[:page]
   end
 end
