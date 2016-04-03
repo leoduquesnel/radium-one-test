d3e6d895408f2613b7479851328e8fd7b29f0a8d
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 19:39:40 -0700

    try some cache on stock tickers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @stock_tickers = StockTicker.select(:id, :name).order('name asc').page params[:page]
+    cache do :stock_tickers, expires_in: 5.minutes do
+      @stock_tickers = StockTicker.select(:id, :name).order('name asc').page params[:page]
+    end
   end
 end
