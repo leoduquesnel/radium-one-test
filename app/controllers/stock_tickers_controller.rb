ea2f45bb05e759660ddb6ecd3c19d11ab179da34
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 20:14:39 -0700

    remove cache form stock prices

--- app/controllers/stock_tickers_controller.rb
+++ app/controllers/stock_tickers_controller.rb
@@ -5,9 +5,7 @@ class StockTickersController < ApplicationController
     
     if stock_ticker.present?
       limit = params[:limit].to_i > 0 ? params[:limit] : 30
-      cache @stock_prices do
-        @stock_prices = stock_ticker.stock_prices.select(:price, :date).order('date desc').limit(limit).to_a
-      end
+      @stock_prices = stock_ticker.stock_prices.select(:price, :date).order('date desc').limit(limit).to_a
     end
 
     respond_to do |format|
