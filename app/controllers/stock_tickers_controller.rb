bc9a7d529cacb5c53b7140c3e1896f1749949d58
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 20:09:51 -0700

    add cache on stock prices

--- app/controllers/stock_tickers_controller.rb
+++ app/controllers/stock_tickers_controller.rb
@@ -5,7 +5,9 @@ class StockTickersController < ApplicationController
     
     if stock_ticker.present?
       limit = params[:limit].to_i > 0 ? params[:limit] : 30
-      @stock_prices = stock_ticker.stock_prices.select(:price, :date).order('date desc').limit(limit)
+      cache @stock_prices do
+        @stock_prices = stock_ticker.stock_prices.select(:price, :date).order('date desc').limit(limit).to_a
+      end
     end
 
     respond_to do |format|
