cdd0b26aed659fb05f2f400b8ad5080fbe152778
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-02 19:41:27 -0700

    type in controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    cache do :stock_tickers, expires_in: 5.minutes do
+    cache :stock_tickers, expires_in: 5.minutes do
       @stock_tickers = StockTicker.select(:id, :name).order('name asc').page params[:page]
     end
   end
